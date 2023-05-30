23f371ff84f6c546eed579e4e116e24ee051a061
samuel <samuel.psanttos@gmail.com>
2023-05-30 15:29:05 -0300

    fix code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
-const { Router } = require("express");
-const userRoutes = require("./users.routes");
+const { Router } = require("express")
+const userRoutes = require("./users.routes")
 const routes = Router();
 
-routes.use("/users", userRoutes);
+routes.use("/users", userRoutes)
 
-module.exports = routes;
+module.exports = routes
